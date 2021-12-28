dd6a480017c117ba9f32d03074149822ea0060bc
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2021-12-28 16:40:03 -0300

    Fix colorscheme setter description

--- lua/csj/colorscheme.lua
+++ lua/csj/colorscheme.lua
@@ -1,8 +1,9 @@
--- Some notes: The hour would be determined by the first 4 characters, so for 04:00 PM you would say 1600000000, and you
--- the the number to be 10 characters long, since os.time() returns that, to change it exactly you would need to print
--- on the command mode this :lua lua print(os.time())
--- The logic would be something like, if the time of the OS is beyond the 10 characters long number then it will set
--- neovim to the dark mode, in this example I use the rose-pine colorscheme
+-- Some notes: The hour is going to be determined by the first 4 characters, so for 04:00 PM you would use 1600000000,
+-- and you need to set the numbers to be exactly 10 characters long, since os.time() returns that, to change it exactly
+-- you would need to print on the command mode this :lua lua print(os.time()).  The logic would be something like, if
+-- the time of the OS is above the value that you gave then it will set neovim to the dark mode, in this example I use
+-- the rose-pine colorscheme
+
 if os.time() >= 1800000000 then
     vim.cmd([[
         try
