084eed65fc5bc54ab45107db2cee7a92b527b074
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2021-12-28 17:59:57 -0300

    Switch colorscheme based on hour working. I hope

--- lua/csj/colorscheme.lua
+++ lua/csj/colorscheme.lua
@@ -1,30 +1,35 @@
-if os.date('%H:%M') >= '18:00' then
-    vim.cmd([[
-        try
-            set background=dark
-            colorscheme rose-pine
-        catch /^Vim\%((\a\+)\)\=:E185/
-            colorscheme default
-        endtry
-    ]])
-else
-    vim.g.rose_pine_variant = 'dawn'
-    vim.cmd([[
-        try
-            set background=light
-            colorscheme rose-pine
-        catch /^Vim\%((\a\+)\)\=:E185/
-            colorscheme default
-        endtry
-    ]])
+local noon_afternoon = '18:00' -- After 6PM set dark mode
+local nigth_morning = '09:00' -- Keep the dark mode till 9AM
+
+local function set_colorscheme(time_of_the_day) -- If 'time' is true the function will return settings for a dark theme
+    if time_of_the_day == true then
+        -- Settings for a dark colorscheme
+        vim.cmd([[
+            try
+                set background=dark
+                colorscheme rose-pine
+            catch /^Vim\%((\a\+)\)\=:E185/
+                colorscheme default
+            endtry
+        ]])
+    elseif time_of_the_day == false then
+        -- Settings for a light colorscheme
+        vim.g.rose_pine_variant = 'dawn'
+        vim.cmd([[
+            try
+                set background=light
+                colorscheme rose-pine
+            catch /^Vim\%((\a\+)\)\=:E185/
+                colorscheme default
+            endtry
+        ]])
+    end
 end
 
--- vim.cmd([[
---     try
---         set background=dark
---         colorscheme rose-pine
---     catch /^Vim\%((\a\+)\)\=:E185/
---         colorscheme default
---         set background=dark
---     endtry
--- ]])
+local actual_time = os.date('%H:%M')
+
+if (actual_time > noon_afternoon or actual_time < nigth_morning) then
+    set_colorscheme(true)
+elseif actual_time > nigth_morning then
+    set_colorscheme(false)
+end
