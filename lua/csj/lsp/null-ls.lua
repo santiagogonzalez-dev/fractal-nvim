3103117262cd6ad70d38921cf45c23ee5e435f40
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-05-22 13:16:19 -0300

    Updates null-ls

--- lua/csj/lsp/null-ls.lua
+++ lua/csj/lsp/null-ls.lua
@@ -1,7 +1,22 @@
-require('null-ls').setup({
+local ok, null = pcall(require, 'null-ls')
+if not ok then
+   return
+end
+
+local format = null.builtins.formatting
+local diag = null.builtins.diagnostics
+local actions = null.builtins.code_actions
+local completion = null.builtins.completion
+
+null.setup({
    sources = {
-      require('null-ls').builtins.formatting.stylua,
-      require('null-ls').builtins.formatting.prettier.with({
+      actions.gitsigns,
+      actions.eslint,
+      diag.eslint,
+      actions.shellcheck,
+      diag.shellcheck,
+      format.stylua,
+      format.prettier.with({
          prefer_local = 'node_modules/.bin',
          command = 'prettier',
          filetypes = {
@@ -12,7 +27,6 @@ require('null-ls').setup({
             'vue',
             'css',
             'scss',
-            'less',
             'html',
             'json',
             'jsonc',
@@ -31,7 +45,23 @@ require('null-ls').setup({
             '80',
          },
       }),
-      require('null-ls').builtins.diagnostics.eslint,
-      require('null-ls').builtins.completion.spell,
+      format.black.with({
+         prefer_local = '.venv/bin',
+         extra_args = {
+            '--fast',
+            '--quiet',
+            -- '--skip-string-normalization',
+            -- '--line-length',
+            -- '88',
+            '--target-version',
+            'py310',
+         },
+      }),
+      diag.flake8.with({
+         prefer_local = '.venv/bin',
+         extra_args = { '--max-line-lenth', '88' },
+      }),
+      diag.eslint,
+      completion.spell,
    },
 })
