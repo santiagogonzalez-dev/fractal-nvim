210ee6cf70349332dfc516adb4038ac01348f269
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-08-29 17:14:50 -0300

    chore: stylua

--- user/plugins/init.lua
+++ user/plugins/init.lua
@@ -1,6 +1,6 @@
 -- LSP, Completion with CMP, Treesitter, Telescope, autopairs, comments and all
 -- the other plugins that I use.
 
-require 'plugins.packer'.setup()
+require('plugins.packer').setup()
 pcall(require, 'plugins.packer_compiled')
 vim.cmd.doautocmd 'User LoadPlugins'
