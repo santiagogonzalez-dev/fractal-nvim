72f5e9cd20365f0d3b372c82d4d7004c181515ff
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-09-12 22:29:45 -0300

    Updates

--- lua/csj/core/init.lua
+++ lua/csj/core/init.lua
@@ -4,25 +4,30 @@ require 'csj.core.autocmds'
 
 do
    local utils = require 'csj.utils'
+   local notify_on_error = require('csj.modules.notifications').notify_send
    local config_path = vim.fn.stdpath 'config' -- "${XDG_CONFIG_HOME}/nvim"
 
    local user_settings_table = string.format('%s%s', config_path, '/user/settings.json')
-   local ok, user = utils.get_json(user_settings_table) -- Get user prefs.
-   if not ok then
-      vim.notify 'CSJNeovim is not able to find settings.json'
+   if not utils.readable(user_settings_table) then
+      notify_on_error 'CSJNeovim is not able to locate `settings.json`.'
    else
-      local start = require 'csj.utils.start'
+      local ok, user = utils.get_json(user_settings_table) -- Get user prefs.
+      if not ok then
+         notify_on_error 'CSJNeovim is not able to read `settings.json` properly.'
+      else
+         local start = require 'csj.utils.start'
 
-      start.colorscheme(user.colorscheme) -- Apply colorscheme.
-      start.conditionals(user.conditionals) -- Conditionals to load plugins and modules.
-      start.modules(user.modules) -- Load modules specified by the user.
-      start.session(user.restore) -- Restore cursor position and view(folds, searches).
-      start.opts(user.opts) -- Set global settings defined by the user.
+         start.colorscheme(user.colorscheme) -- Apply colorscheme.
+         start.conditionals(user.conditionals) -- Conditionals to load plugins and modules.
+         start.modules(user.modules) -- Load modules specified by the user.
+         start.session(user.restore) -- Restore cursor position and view(folds, searches).
+         start.opts(user.opts) -- Set global settings defined by the user.
+      end
    end
 
    local user_init = string.format('%s%s', config_path, '/user/init.lua')
    if not utils.readable(user_init) then
-      vim.notify 'CSJNeovim is not able to find an init.lua for user'
+      notify_on_error 'CSJNeovim is not able to find an `init.lua` for user.'
    else
       -- Add `./user` to lua path, do this before calling user's `init.lua`.
       package.path = table.concat {
