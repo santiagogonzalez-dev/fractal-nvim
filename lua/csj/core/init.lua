6178b737339b821233863ae53f84b0fd6fce173f
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-09-12 21:59:37 -0300

    Updates

--- lua/csj/core/init.lua
+++ lua/csj/core/init.lua
@@ -1,26 +1,30 @@
--- Default settings
+-- Default settings.
 require 'csj.core.general'
 require 'csj.core.autocmds'
 
 do
    local utils = require 'csj.utils'
    local config_path = vim.fn.stdpath 'config' -- "${XDG_CONFIG_HOME}/nvim"
-   local user_settings = string.format('%s%s', config_path, '/user/settings.json')
-   if utils.readable(user_settings) then
-      local user = require('csj.utils').get_json(user_settings) -- User table
+
+   local user_settings_table = string.format('%s%s', config_path, '/user/settings.json')
+   if not utils.readable(user_settings_table) then
+      vim.notify 'CSJNeovim is not able to find settings.json'
+   else
+      local user = utils.get_json(user_settings_table) -- Get user prefs.
       local start = require 'csj.utils.start'
+
       start.colorscheme(user.colorscheme) -- Apply colorscheme.
       start.conditionals(user.conditionals) -- Conditionals to load plugins and modules.
       start.modules(user.modules) -- Load modules specified by the user.
-      start.session(user.restore) -- Restore cursor and view.
-      start.opts(user.opts) -- Set some settings.
-   else
-      vim.notify 'CSJNeovim is not able to find settings.json'
+      start.session(user.restore) -- Restore cursor position and view(folds, searches).
+      start.opts(user.opts) -- Set global settings defined by the user.
    end
 
    local user_init = string.format('%s%s', config_path, '/user/init.lua')
-   if utils.readable(user_init) then
-      -- Add `./user` to lua path, do this before calling user's init.lua
+   if not utils.readable(user_init) then
+      vim.notify 'CSJNeovim is not able to find an init.lua for user'
+   else
+      -- Add `./user` to lua path, do this before calling user's init.lua.
       package.path = table.concat {
          package.path,
          ';',
@@ -30,8 +34,6 @@ do
          '/user/?/init.lua;',
       }
 
-      dofile(user_init) -- User's init.lua
-   else
-      vim.notify 'CSJNeovim is not able to find an init.lua for user'
+      dofile(user_init) -- User's init.lua.
    end
 end
