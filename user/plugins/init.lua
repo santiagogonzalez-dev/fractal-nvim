5adb2a9c6bc7ea94dab06fa37fb9eb0b626bae33
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-10-01 12:43:56 -0300

    Deferring the plugins makes spellsitter fail

--- user/plugins/init.lua
+++ user/plugins/init.lua
@@ -4,11 +4,13 @@
 require('plugins.packer').setup()
 pcall(require, 'plugins.packer_compiled')
 
-vim.api.nvim_create_autocmd('UIEnter', {
-   callback = function()
-      vim.defer_fn(function()
-         vim.cmd.doautocmd('User LoadPlugins')
-         vim.cmd(':e')
-      end, 30)
-   end,
-})
+-- vim.api.nvim_create_autocmd('UIEnter', {
+--    callback = function()
+--       vim.defer_fn(function()
+--          vim.cmd.doautocmd('User LoadPlugins')
+--          vim.cmd(':e')
+--       end, 30)
+--    end,
+-- })
+
+vim.cmd.doautocmd('User LoadPlugins')
