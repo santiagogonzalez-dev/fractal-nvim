d9fe610d1cf147c9feee6a2d7cef7030be6fb8bd
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-11-08 05:33:25 -0300

    Separated inits

---
+++ lazy.lua
@@ -0,0 +1,48 @@
+require "fractal.core.not"
+
+-- Temporarily disable ftplugin and shadafile.
+vim.cmd.filetype "off"
+vim.cmd.filetype "plugin indent off"
+vim.opt.shadafile = "NONE"
+
+-- Enable bundled treesitter parsers, this also turns off `syntax`.
+-- https://github.com/neovim/neovim/issues/14090#issuecomment-1237820552
+vim.g.ts_highlight_lua = true
+
+-- PROFILING: About profiling, all the settings before this comment are going
+-- to be run first, then everything under `plugins`, then `after/plugins`, and
+-- lastly the main function will be scheduled and run so that we defer the load
+-- of the config.
+vim.schedule(function()
+   if not vim.opt.loadplugins:get() then
+      vim.cmd "runtime! plugin/**/*.vim"
+      vim.cmd "runtime! plugin/**/*.lua"
+   end
+
+   -- Enable filetype settings and shadafile.
+   vim.cmd.filetype "on"
+   vim.cmd.filetype "plugin indent on"
+   vim.opt.shadafile = ""
+   vim.cmd.rshada({ bang = true })
+
+   local buf_curr_path = vim.fn.expand "%F"
+   local utils = require "fractal.utils"
+   if not utils.readable(buf_curr_path) then
+      -- Manually trigger autocmd, necessary for skeletons.
+      vim.defer_fn(function()
+         vim.api.nvim_exec_autocmds("BufNewFile", {})
+      end, 0)
+   else
+      -- Manually call ftplugin.
+      vim.defer_fn(function()
+         vim.cmd.filetype "detect"
+      end, 0)
+   end
+
+   vim.defer_fn(function()
+      vim.api.nvim_exec_autocmds("BufEnter", {})
+      vim.api.nvim_exec_autocmds("UIEnter", {})
+   end, 0)
+
+   require "fractal.core"
+end)
