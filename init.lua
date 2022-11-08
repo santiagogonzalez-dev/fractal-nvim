0ac4afc06c155a5e841564575f243cf555174998
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-11-07 21:30:30 -0300

    Maintenance

--- init.lua
+++ init.lua
@@ -1,39 +1,4 @@
--- Disable builtin plugins.
-vim.opt.loadplugins = false
-
-vim.g.loaded_2html_plugin = false
-vim.g.loaded_bugreport = false
-vim.g.loaded_compiler = false
-vim.g.loaded_getscript = false
-vim.g.loaded_getscriptPlugin = false
-vim.g.loaded_gzip = false
-vim.g.loaded_logiPat = false
-vim.g.loaded_logipat = false
-vim.g.loaded_man = false
-vim.g.loaded_matchParen = false
-vim.g.loaded_matchit = false
-vim.g.loaded_netrwFileHandlers = false
-vim.g.loaded_netrwSettings = false
-vim.g.loaded_optwin = false
-vim.g.loaded_perl_provider = false
-vim.g.loaded_rplugin = false
-vim.g.loaded_rrhelper = false
-vim.g.loaded_spec = false
-vim.g.loaded_synmenu = false
-vim.g.loaded_syntax = false
-vim.g.loaded_tar = false
-vim.g.loaded_tarPlugin = false
-vim.g.loaded_tutor = false
-vim.g.loaded_vimball = false
-vim.g.loaded_vimballPlugin = false
-vim.g.loaded_zip = false
-vim.g.loaded_zipPlugin = false
-
--- Disable language providers.
-vim.g.loaded_python3_provider = false
-vim.g.loaded_node_provider = false
-vim.g.loaded_ruby_provider = false
-vim.g.loaded_perl_provider = false
+require "fractal.core.not"
 
 -- Temporarily disable ftplugin and shadafile.
 vim.cmd.filetype "off"
@@ -60,9 +25,9 @@ vim.schedule(function()
    vim.opt.shadafile = ""
    vim.cmd.rshada({ bang = true })
 
-   local curr_buf = vim.fn.expand "%F"
+   local buf_curr_path = vim.fn.expand "%F"
    local utils = require "fractal.utils"
-   if not utils.readable(curr_buf) then
+   if not utils.readable(buf_curr_path) then
       -- Manually trigger autocmd, necessary for skeletons.
       vim.defer_fn(function()
          vim.api.nvim_exec_autocmds("BufNewFile", {})
--- lua/fractal/core/init.lua
+++ lua/fractal/core/init.lua
@@ -2,14 +2,14 @@ local utils = require "fractal.utils"
 local check, get_json, readable = utils.check, utils.get_json, utils.readable
 local ROOT = vim.fn.stdpath "config" -- "${XDG_CONFIG_HOME}/nvim"
 local fract = require "fractal.core.configuration"
-local user_init = string.format("%s%s", ROOT, "/user/init.lua")
 
 -- Basic settings.
 require "fractal.core.general"
 require "fractal.core.general.autocmds"
 
+local fract_settings = string.format("%s%s", ROOT, "/user/fractal.json")
 check({
-   eval = get_json(string.format("%s%s", ROOT, "/user/fractal.json")),
+   eval = get_json(fract_settings),
    on_fail_msg = "Not able to locate `fractal.json`.",
    callback = function(CFG)
       fract.colorscheme(CFG.colorscheme) -- Apply colorscheme.
@@ -29,6 +29,7 @@ package.path = table.concat({
    "/user/?/init.lua;",
 })
 
+local user_init = string.format("%s%s", ROOT, "/user/init.lua")
 check({
    eval = readable(user_init),
    on_fail_msg = "Not able to find an `init.lua` for user.",
