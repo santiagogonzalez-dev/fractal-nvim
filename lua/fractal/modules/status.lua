e3d23f7c9ff31c85a1644d26c5dbe4e1773b7a9d
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-01-04 19:00:37 -0300

    updates

--- lua/fractal/modules/status.lua
+++ lua/fractal/modules/status.lua
@@ -3,6 +3,7 @@ local M = {
    -- if you set it up with `hide-completely` you'll not see it, `basic` gives
    -- you a basic statusline.
 }
+
 local data = require "fractal.utils.data"
 
 -- TODO(santigo-zero): Generate hl groups
@@ -20,10 +21,10 @@ function M.get()
       data.current_keys(),
       "%#StatusLineBlue#", -- Reset hl groups
       " ",
-      data.search_count(),
+      -- data.search_count(),
       "%#StatusLine#", -- Reset hl groups
       "%=",
-      data.filepath(),
+      -- data.filepath(),
       "%#StatusLineBlue#",
       data.filename(),
       "%#StatusLine#", -- Reset hl groups
@@ -42,6 +43,7 @@ function M.hide_completely()
 end
 
 function M.setup(mode)
+   vim.opt.cmdheight = 0
    if mode == "hide-completely" then
       -- vim.api.nvim_set_hl(0, 'StatusLine', { link = 'Normal' })
       -- vim.api.nvim_set_hl(0, 'StatusLineNC', { link = 'Normal' })
@@ -49,19 +51,25 @@ function M.setup(mode)
          '%{%v:lua.require("fractal.modules.status").hide_completely()%}'
       vim.opt.laststatus = 0
       vim.opt.ruler = false
-      vim.opt.cmdheight = 0
       vim.api.nvim_set_hl(0, "EndOfBuffer", { link = "Normal" })
    elseif mode == "basic" then
       vim.opt.laststatus = 3
       vim.opt.statusline = '%{%v:lua.require("fractal.modules.status").get()%}'
-      vim.api.nvim_create_autocmd({ "TabEnter", "BufEnter", "WinEnter" }, {
+      vim.api.nvim_create_autocmd({
+         "TabEnter",
+         "BufEnter",
+         "WinEnter",
+      }, {
          callback = function()
             vim.opt.statusline =
                '%{%v:lua.require("fractal.modules.status").get()%}'
          end,
       })
 
-      vim.api.nvim_create_autocmd({ "WinEnter", "FileType" }, {
+      vim.api.nvim_create_autocmd({
+         "WinEnter",
+         "FileType",
+      }, {
          pattern = {
             "NetrwTreeListing",
             "TelescopePrompt",
