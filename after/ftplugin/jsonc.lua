5d3a0541ce8648f5cbdee1b390618dd3aad4689e
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-04-19 13:23:01 -0300

    Insert trailing comma in json and jsonc
    Insert double quotes on attributes in svelte and html

--- after/ftplugin/jsonc.lua
+++ after/ftplugin/jsonc.lua
@@ -4,3 +4,15 @@ vim.opt_local.shiftwidth = tab_lenght
 vim.opt_local.colorcolumn = '80,120'
 vim.opt_local.textwidth = 80
 vim.opt_local.conceallevel = 2
+
+-- Add trailing comma
+vim.keymap.set('n', 'o', function()
+   local line = vim.api.nvim_get_current_line()
+
+   local should_add_comma = string.find(line, '[^,{[]$')
+   if should_add_comma then
+      return 'A,<cr>'
+   else
+      return 'o'
+   end
+end, { buffer = true, expr = true })
