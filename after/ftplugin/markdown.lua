0916c5953df36abc8f7b485a3bc791b3c53ba1ac
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-07-01 00:43:00 -0300

    Don't use autocmd for spelling in markdown files

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -4,3 +4,8 @@ vim.opt_local.shiftwidth = tab_lenght
 vim.opt_local.colorcolumn = '80,120'
 vim.opt_local.textwidth = 80
 vim.opt_local.conceallevel = 2
+vim.opt.spell = true
+
+for _, event in pairs(vim.api.nvim_get_autocmds({ group = '_enable_spell' })) do
+	vim.api.nvim_del_autocmd(event.id)
+end
