14ea58878ca4e3e19a5a980dba106136c4073e26
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-07-13 22:26:22 -0300

    update marksman and rust tools

--- user/plugins/lazy.lua
+++ user/plugins/lazy.lua
@@ -291,6 +291,34 @@ local plugins = {
 			require("colorizer").attach_to_buffer(0, { css = true, svelte = true })
 		end,
 	},
+
+	{
+		"simrat39/rust-tools.nvim",
+		config = function()
+			local rt = require("rust-tools")
+
+			rt.setup({
+				server = {
+					on_attach = function(_, bufnr)
+						-- Hover actions
+						vim.keymap.set(
+							"n",
+							"<C-space>",
+							rt.hover_actions.hover_actions,
+							{ buffer = bufnr }
+						)
+						-- Code action groups
+						vim.keymap.set(
+							"n",
+							"<Leader>a",
+							rt.code_action_group.code_action_group,
+							{ buffer = bufnr }
+						)
+					end,
+				},
+			})
+		end,
+	},
 }
 
 require("lazy").setup(plugins)
