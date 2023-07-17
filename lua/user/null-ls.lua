8f1d4658f85561870e8f333dab2da6e8c5a06be8
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-07-17 19:37:55 -0300

    more updates, add neogit

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -3,13 +3,56 @@ local M = {
 }
 
 function M.config()
-	local null_ls = require("null-ls")
+	local null = require("null-ls")
 
-	null_ls.setup({
+	local format = null.builtins.formatting
+	local diag = null.builtins.diagnostics
+	local actions = null.builtins.code_actions
+
+	null.setup({
 		sources = {
-			null_ls.builtins.formatting.stylua,
-			null_ls.builtins.diagnostics.eslint,
-			null_ls.builtins.completion.spell,
+			actions.gitsigns,
+			diag.eslint,
+			actions.shellcheck,
+			diag.shellcheck,
+			diag.luacheck,
+			format.stylua,
+			format.prettier.with({
+				prefer_local = "node_modules/.bin",
+				command = "prettier",
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"typescript",
+					"typescriptreact",
+					"vue",
+					"css",
+					"scss",
+					"html",
+					"json",
+					"jsonc",
+					"yaml",
+					"markdown",
+					"graphql",
+					"handlebars",
+				},
+			}),
+			format.black.with({
+				prefer_local = ".venv/bin",
+				extra_args = {
+					"--fast",
+					"--quiet",
+					-- '--skip-string-normalization',
+					-- "--line-length",
+					-- "88",
+					-- "--target-version",
+					-- "py310",
+				},
+			}),
+			diag.flake8.with({
+				prefer_local = ".venv/bin",
+				-- extra_args = { "--max-line-lenth", "88" },
+			}),
 		},
 	})
 end
