da1b7c96354145740378ac4e0b5a310dc64f0305
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-08-25 08:02:11 -0300

    icons

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,14 +1,17 @@
 vim.loader.enable()
 
+require("user.launch")
+
+require("user.modules.icons")
+spec("user.modules.devicons")
+
 require("user.config.settings")
 require("user.config.keymaps")
 require("user.config.gutter")
 require("user.config.hidden-statusline")
-
-require("user.launch")
-
 spec("user.modules.colorscheme")
 spec("user.modules.rcd")
+
 spec("user.modules.comment")
 spec("user.modules.surround")
 spec("user.modules.accelerated-jk")
