ccae5bf6f669cb63a34f756cc3a5c094b521a55e
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2023-11-02 21:58:18 -0300

    feat: Remove nu custom ts parser

--- lua/user/modules/treesitter.lua
+++ lua/user/modules/treesitter.lua
@@ -156,16 +156,6 @@ function M.config()
 			},
 		},
 	})
-	local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-	parser_config.nu = {
-		install_info = {
-			url = "https://github.com/nushell/tree-sitter-nu",
-			files = { "src/parser.c" },
-			branch = "main",
-		},
-		filetype = "nu",
-	}
 end
 
 return M
